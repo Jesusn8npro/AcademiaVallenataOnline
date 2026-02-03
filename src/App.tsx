b54dde30e26b604e046d401cdc503304dec468d0
Tu Nombre Aquí <tu-email@ejemplo.com>
2026-02-02 23:49:25 -0500

    fix: corregir importación de App.css (case sensitivity) para deploy en linux

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './app.css'
+import './App.css'
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
 import Home from './Paginas/Inicio/Home'
